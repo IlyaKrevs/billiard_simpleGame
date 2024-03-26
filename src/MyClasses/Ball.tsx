6f6051260f68e4582c6e83c87edde65030eda0dd
Krevs <m-u-r-z-a@mail.ru>
2024-03-26 06:25:02 +0500

    try to create class

---
+++ src/MyClasses/Ball.tsx
@@ -0,0 +1,62 @@
+interface IBall {
+    id: number,
+    pos: {
+        x: number,
+        y: number,
+    },
+    radius: number,
+    color: string,
+    speed: {
+        dx: number,
+        dy: number,
+    }
+}
+
+class Ball implements IBall {
+    id: number;
+    pos: { x: number; y: number; };
+    radius: number;
+    color: string;
+    speed: { dx: number; dy: number; };
+
+    constructor({ pos, radius, color, speed }: IBall) {
+        this.id = Ball.myCounter()
+        this.pos = pos;
+        this.radius = radius
+        this.color = color
+        this.speed = speed
+    }
+
+    setColor(value: string) {
+        this.color = value
+    }
+
+    setSpeed(speed: { dx: number, dy: number }) {
+        this.speed.dx = speed.dx
+        this.speed.dy = speed.dy
+    }
+
+    oneMove() {
+        if (this.speed.dx !== 0 || this.speed.dy !== 0) {
+            this.pos.x = this.pos.x + this.speed.dx
+            this.pos.y = this.pos.x + this.speed.dy
+            this.slowDown()
+        }
+    }
+
+    slowDown(percent: number = 0.95, stopFactor = 0.05) {
+        if (Math.abs(this.speed.dx) > stopFactor || Math.abs(this.speed.dy) > stopFactor) {
+            this.speed.dx = this.speed.dx * percent
+            this.speed.dy = this.speed.dy * percent
+        }
+    }
+
+    static myCounter: () => number = (function (): () => number {
+        let counter: number = 0;
+        return function (): number {
+            return ++counter;
+        };
+    })();
+}
+
+export default Ball
